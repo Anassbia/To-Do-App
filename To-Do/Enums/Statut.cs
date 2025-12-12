37b861cc590797a105d61e52a0309efad2100188
Anas Sbia <sbiaanas00@gmail.com>
2025-12-12 21:18:56 +0100

    Add project files.

---
+++ To-Do/Enums/Statut.cs
@@ -0,0 +1,10 @@
+﻿namespace To_Do.Enums
+{
+    public enum Statut
+    {
+        TODO=0,
+        DOING=1,
+        DONE=2
+
+    }
+}
---
+++ To-Do/ViewModels/TodoAddVM.cs
@@ -0,0 +1,11 @@
+﻿namespace To_Do.ViewModels
+{
+    public class TodoAddVM
+    {
+        public string Libelle { get; set; }
+        public string Description { get; set; }
+        public DateTime DateLimite { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
