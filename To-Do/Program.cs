dc40a2dbd6f5613520babe624bef53592e220baf
Anas Sbia <sbiaanas00@gmail.com>
2025-12-13 22:47:11 +0100

    Add:file logiing

--- To-Do/Program.cs
+++ To-Do/Program.cs
@@ -1,32 +1,49 @@
 using To_Do.Filters;
 using To_Do.Interfaces;
 using To_Do.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// serilog config
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+
+    // removin .net logs
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
+
+    //  App logs
+    .WriteTo.Console()
+    .WriteTo.File(
+        "Logs/todo-log.txt",
+        rollingInterval: RollingInterval.Day)
+
+    .CreateLogger();
+
+
+builder.Host.UseSerilog();
+
+
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
 
-builder.Services.AddScoped<ITodoService,TodoService>();
+builder.Services.AddScoped<ITodoService, TodoService>();
 builder.Services.AddScoped<SessionManagerService>();
-
 builder.Services.AddScoped<ThemeFilter>();
 
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add<ThemeFilter>();
-});//globale
-
-
+}); // global filter
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -35,12 +52,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 app.UseSession();
+
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Todo}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
